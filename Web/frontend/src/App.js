6e2aa5aba786590b1274dfa5e75ab5c265be55b1
Akshay Raichur <lordakshay99@gmail.com>
2020-05-18 17:50:44 +0530

    Added routes to frontend

--- Web/frontend/src/App.js
+++ Web/frontend/src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import { Register } from "./Screens/Register/Register";
 import { Signin } from "./Screens/Signin/Signin";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavBar />
-      <Signin />
-    </div>
+      <Switch>
+        <Route path="/signin" exact>
+          <Signin />
+        </Route>
+
+        <Route path="/register" exact>
+          <Register />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
